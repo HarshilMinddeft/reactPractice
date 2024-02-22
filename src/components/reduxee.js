8765e023dcd682c91d616c97cc7ebbb18cea0e95
HarshilMinddeft <harshil.mandali@minddeft.net>
2024-02-22 17:45:42 +0530

    abc

---
+++ src/components/reduxee.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Count from "./utils/count";
+import { useDispatch } from "react-redux";
+const Reduxee = () => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <button onClick={(e) => dispatch({ type: "Increment" })}>Increase</button>
+      <Count />
+      <button onClick={(e) => dispatch({ type: "Decrement" })}>Decrease</button>
+    </div>
+  );                                                    
+};
+
+export default Reduxee;
